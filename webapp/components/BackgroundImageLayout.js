e9c5d950bbf897fe314231ae7574d85dd61324cc
pierrefg <pierrefg@proton.me>
2024-10-15 15:02:44 +0200

    Optimizes performance

--- webapp/components/BackgroundImageLayout.js
+++ webapp/components/BackgroundImageLayout.js
@@ -5,21 +5,18 @@ import AnimatedText from "./animatedText";
 export default function BackgroundImageLayout({children, icon, title, coverPic}) {
     return (
         <>
-            <div className="relative w-full min-h-[300px] flex items-center justify-center">
+            <div className="relative min-h-[300px] flex items-center justify-center">
                 <div className="absolute inset-0">
                     <Image
                         src={coverPic}
-                        alt="Background image"
+                        alt="cover image"
                         layout="fill"
                         objectFit="cover"
                         objectPosition="center"
                         className="opacity-30"
-                        placeholder={'blur'}
                         priority
                     />
                 </div>
-
-                <div className="absolute inset-0bg-opacity-50"></div>
 
                 <div className="relative z-10 max-w-4xl p-6">
                     <h1 className="text-2xl md:text-3xl mb-6">
