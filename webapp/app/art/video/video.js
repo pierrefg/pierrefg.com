3dd02118919fc3ed604a12b94e938ec06cb21d47
Pierre FG <pierrefg@proton.me>
2024-10-17 15:50:04 +0200

    Adds cube video

--- webapp/app/art/video/video.js
+++ webapp/app/art/video/video.js
@@ -1,90 +1,34 @@
 'use client';
 
-import SimpleLink from "@/components/SimpleLink";
-
-const videoData = [
-    {
-        type: 'Clip musical',
-        title: 'Forbidden Fruit par Lori Pearlson (feat. Lea FM)',
-        year: 2020,
-        link: 'https://www.youtube.com/watch?v=MjLQH9F9fXs'
-    },
-    {
-        type: 'Court métrage',
-        title: 'La Petite Omelette',
-        year: 2019,
-        link: 'https://www.youtube.com/watch?v=YuKHhfBkRyA'
-    },
-    {
-        type: 'After movie',
-        title: 'WEIFA',
-        year: 2019,
-        link: 'https://www.youtube.com/watch?v=6bVek6l4Giw'
-    },
-    {
-        type: 'After movie',
-        title: 'IF50',
-        year: 2019,
-        link: 'https://www.youtube.com/watch?v=xhrxCQEs32E'
-    },
-    {
-        type: 'Rapport',
-        title: 'Rapport de stage de deuxième année (lauréat du prix Gaston Berger)',
-        year: 2016,
-        link: 'https://www.youtube.com/watch?v=RzpSbEcSk-A'
-    },
-    {
-        type: 'Clip musical',
-        title: 'My Queen par Universal Mind',
-        year: 2015,
-        link: 'https://www.youtube.com/watch?v=YqZEP1rHAII'
-    },
-    {
-        type: 'Rapport',
-        title: 'TPE (Travaux Pratiques Encadrés)',
-        year: 2013,
-        link: 'https://www.youtube.com/watch?v=81QvlGe7Ga4&t=278s'
-    },
-    {
-        type: 'Court métrage',
-        title: 'Le Labyrinthe',
-        year: 2013,
-        link: 'https://www.youtube.com/watch?v=K-DaBf7izT0'
-    },
-    {
-        type: 'Court métrage',
-        title: 'Op.15 n°7',
-        year: 2013,
-        link: 'https://www.youtube.com/watch?v=tTA_3U4B0JE'
-    },
-    {
-        type: 'Court métrage',
-        title: "L'invention",
-        year: 2012,
-        link: 'https://www.youtube.com/watch?v=qRPdk6Llqes'
-    },
-]
-
 export default function Video() {
     return (
-        <div className="flex flex-col gap-2 mx-auto px-4 w-fit">
-            {
-                videoData.map(
-                    (item, index) => (
-                        <div key={index}>
-                            {item.year} - [{item.type}] - <i>{item.title}</i><> </>
-                            <a
-                                href={item.link} 
-                                className="btn btn-secondary" 
-                                target="_blank" 
-                                rel="noopener noreferrer"
-                            >
-                                Voir
-                            </a>
-                        </div>
+        <div className="flex flex-col gap-10 mx-auto px-4 w-full max-w-screen-md">
+            <div className="video-container max-w-md mx-auto">
+                <video className="w-full h-auto" controls>
+                    <source src="https://of5ylsz4mar7xvsj.public.blob.vercel-storage.com/video/cube.mp4" type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+
+            <div className="flex flex-col gap-2">
+                {
+                    videoData.map(
+                        (item, index) => (
+                            <div key={index}>
+                                {item.year} - [{item.type}] - <i>{item.title}</i><> </>
+                                <a
+                                    href={item.link} 
+                                    className="btn btn-secondary" 
+                                    target="_blank" 
+                                    rel="noopener noreferrer"
+                                >
+                                    Voir
+                                </a>
+                            </div>
+                        )
                     )
-                )
-            }
+                }
+            </div>
         </div>
     );
 }
