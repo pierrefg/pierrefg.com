24d07250805320a02c13a6eaa39e2ebf6396f4c4
Pierre FG <p1ps@pierres-macbook-pro.home>
2026-05-15 14:43:56 +0200

    Updates music section description

--- webapp/app/art/music/music.js
+++ webapp/app/art/music/music.js
@@ -8,7 +8,7 @@ export default function Music() {
     return (
         <div className="flex flex-col gap-6">
             <p className="text-center max-w-sm mx-auto py-5">
-                Voici 2 de mes projets, quelque chose de plus gros se prépare en secret, et peut-être pas tout seul {">.<"} 
+                Quelques-un de mes projets persos, quelque chose de plus gros se prépare en secret, et peut-être pas tout seul {">.<"} 
             </p>
             <PlaylistPlayer playlist={laDerniereVague} />
             <PlaylistPlayer playlist={echoesFromTheGarden} />
