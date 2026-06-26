c70355fb1ba57944076df6fb1ee466c3fb41c297
Pierre FG <p1ps@pierres-macbook-pro.home>
2026-06-26 09:20:08 +0200

    Adds new mariage

---
+++ webapp/app/art/photography/content/2026/mariage_zoe/data.js
@@ -0,0 +1,111 @@
+import imgA from './A.jpg';
+import imgB from './B.jpg';
+import imgC from './C.jpg';
+import imgD from './D.jpg';
+import imgE from './E.jpg';
+import imgF from './F.jpg';
+import imgG from './G.jpg';
+import imgH from './H.jpg';
+import imgI from './I.jpg';
+import imgJ from './J.jpg';
+import imgK from './K.jpg';
+import imgL from './L.jpg';
+import imgM from './M.jpg';
+import imgN from './N.jpg';
+import imgO from './O.jpg';
+import imgP from './P.jpg';
+import imgQ from './Q.jpg';
+import imgR from './R.jpg';
+import imgS from './S.jpg';
+import imgT from './T.jpg';
+import imgU from './U.jpg';
+import imgV from './V.jpg';
+import imgW from './W.jpg';
+import imgX from './X.jpg';
+import imgY from './Y.jpg';
+
+export const mariageZoe = {
+    title: 'Mariage Zoé et Tanguy',
+    date: '2026',
+    place: 'Calvados',
+    imgs: [
+        {
+            img: imgV
+        },
+        {
+            img: imgA
+        },
+        {
+            img: imgB
+        },
+        {
+            img: imgC
+        },
+        {
+            img: imgD
+        },
+        {
+            img: imgE
+        },
+        {
+            img: imgF
+        },
+        {
+            img: imgG
+        },
+        {
+            img: imgH
+        },
+        {
+            img: imgI
+        },
+        {
+            img: imgJ
+        },
+        {
+            img: imgK
+        },
+        {
+            img: imgL
+        },
+        {
+            img: imgM
+        },
+        {
+            img: imgN
+        },
+        {
+            img: imgO
+        },
+        {
+            img: imgP
+        },
+        {
+            img: imgQ
+        },
+        {
+            img: imgR
+        },
+        {
+            img: imgS
+        },
+        {
+            img: imgT
+        },
+        {
+            img: imgU
+        },
+        {
+            img: imgV
+        },
+        {
+            img: imgW
+        },
+        {
+            img: imgX
+        },
+        {
+            img: imgY
+        }
+    ]
+}
--- webapp/app/art/photography/content/data.js
+++ webapp/app/art/photography/content/data.js
@@ -35,8 +35,10 @@ import { jvf } from "./2025/journee_violences_femmes/data";
 import { rencurel } from "./2026/rencurel/data";
 import { premierMai26 } from "./2026/1ermai/data";
 import { roman_empire } from "./2026/roman_empire/data";
+import { mariageZoe } from "./2026/mariage_zoe/data";
 
 export const photoData = [
+    mariageZoe,
     premierMai26,
     rencurel,
     roman_empire,
